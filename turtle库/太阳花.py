4f77b1e86a1cea31cb5bd47cc8866a60cf8d58ac
liu-creator <1530375773@qq.com>
2025-06-01 22:33:14 +0800

    Update 太阳花.py
    push to basic2025

--- turtle库/太阳花.py
+++ turtle库/太阳花.py
@@ -38,7 +38,7 @@ def sun_flower():
     turtle.color("red", "yellow")
 
     turtle.begin_fill()
-    for _ in range(6):     # 画36条线
+    for _ in range(6):     # 画36条线30333
         turtle.forward(400)
         turtle.left(170)
     turtle.end_fill()
